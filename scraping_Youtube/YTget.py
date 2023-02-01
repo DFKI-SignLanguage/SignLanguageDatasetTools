a5735ab83da583e38cd5a887c9dfdb6bcd793c08
f4biand3 <fabiandeckert2@gmail.com>
2023-02-01 22:43:37 +0100

    error handling

--- scraping_Youtube/YTget.py
+++ scraping_Youtube/YTget.py
@@ -33,147 +33,166 @@ def createnewtitle(direc,name):
         
 
 def createdata(C_ID, Lang, dia, gran, a, lang_code, cp):
-    
-    p,p_t = geturl(cp, a)
-    
-    if(os.path.exists(p_t) == False):
-        os.mkdir(p_t)
-    else:
-        shutil.rmtree(p_t)
-        os.mkdir(p_t)
-    
-    os.mkdir(os.path.join(p_t , "captions"))
-    os.mkdir(os.path.join(p_t , "videos"))
-    os.mkdir(os.path.join(p_t , "videos_uncropped"))
-    os.mkdir(os.path.join(p_t , "other"))
-    
-    for url in p.video_urls:
-        try:
-            yt = YouTube(url)
-        except:
-            print(f'\nVideo {url} is unavaialable, skipping.')
+    data = data_
+    try:
+        p,p_t = geturl(cp, a)
+        
+        if(os.path.exists(p_t) == False):
+            os.mkdir(p_t)
         else:
-            print(f'\nDownloading video: {url}')
-            
-            #create variable for the title, which can used as filename
-            titel = yt.title.replace("|", "").replace("\\", "") \
-                        .replace("/", "").replace("\"", "").replace("?", "") \
-                        .replace("*", "").replace(":", "").replace(">", "").replace("<", "")
-            
-            titel = createnewtitle(p_t,titel)
-            #download the video
+            rem = input("\nDirectory for this URL already exist. Do you want to remove it? (y/n) ")
+            if(rem == "y"):
+                shutil.rmtree(p_t)
+                os.mkdir(p_t)
+                
+        
+        os.makedirs(os.path.join(p_t , "captions"), exist_ok = True)
+        os.makedirs(os.path.join(p_t , "videos"), exist_ok = True)
+        os.makedirs(os.path.join(p_t , "videos_uncropped"), exist_ok = True)
+        os.makedirs(os.path.join(p_t , "other"), exist_ok = True)
+        
+        for url in p.video_urls:
             try:
-                stream = yt.streams.get_highest_resolution()
-                yt.streams.get_highest_resolution().download(os.path.join(p_t , "videos_uncropped"),titel+".mp4")
+                yt = YouTube(url)
             except:
-                print(f'\nDownloading {url} is unavaialable, skipping.')
-             
-            #download captions
-            l = ""
-            lang_code = lang_code_s
-            while(l == ""):
-                if(lang_code == "" or len(yt.captions) == 0):
-                    break
-                elif(len(yt.captions) == 1):
-                    lang_code = list(yt.captions.lang_code_index.keys())[0]
-                    l = yt.captions[lang_code].xml_captions
-                    d = open(os.path.join(p_t, "captions", titel+"_"+lang_code+".xml"), "x", encoding="utf-8")
-                    d.write(l)
-                    d.close
-                else:
-                    try:
-                        l = yt.captions[lang_code].xml_captions
-                        d = open(os.path.join(p_t, "captions", titel+"_"+lang_code+".xml"), "x", encoding="utf-8")
-                        d.write(l)
-                        d.close
-                    except:
-                        print("\n"+str(yt.captions))
-                        lang_code = input("\nType in the language code for the captions or press enter: ")
-                        
-
-            # append Channel Id -> not codable
-            data.append(C_ID)
-            
-            # append Language -> not codable
-            data.append(Lang)
-            
-            # append Channel Dialect -> not codable
-            data.append(dia)
-            
-            # append Channel Video Name
-            data.append(str(titel))
-            
-            # append Part of Series -> not codable
-            data.append("tbd")
-            
-            # append Url
-            data.append(str(url))
-            
-            # append Release Date
-            data.append(str(YouTube(url).publish_date.strftime("%Y/%m/%d")))
-            
-            # append Resolution
-            data.append(str(stream.resolution))
-            
-            # append Cropped -> not codable
-            data.append("tbd")
-            
-            # append Video Length
-            i = 0
-            while(i < 3):
+                print(f'\nVideo {url} is unavaialable, skipping.')
+            else:
+                print(f'\nDownloading video: {url}')
+                
+                #create variable for the title, which can used as filename
+                titel = yt.title.replace("|", "").replace("\\", "") \
+                            .replace("/", "").replace("\"", "").replace("?", "") \
+                            .replace("*", "").replace(":", "").replace(">", "").replace("<", "")
+                
+                titel = createnewtitle(p_t,titel)
+                
+                vid = os.path.join(p_t , "videos_uncropped",titel+".mp4")
+                if(os.path.exists(vid) == True):
+                    print("Video is already downloaded.")
+                    continue
+                
+                #download the video
                 try:
-                    t = YouTube(url).length
-                    t_form = str(time.strftime("%H:%M:%S", time.gmtime(t)))
-                    i = 3
-                except Exception:
-                    t_form = "tbd"
-                i = i+1    
-            data.append(t_form)
-            
-            # append FPS
-            data.append(str(stream.fps))
-            
-            # append Number of Frames
-            if(t_form != "tbd"):
-                data.append(str(stream.fps*t))
-            else:
-                data.append("tbd")
-            
-            # append SL Subtitles Available -> not codable
-            data.append("tbd")
-            
-            # append Audio Transcript Available -> not codable
-            data.append("tbd")
-            
-            # append Granularity -> not codable
-            data.append(gran)
-            
-            # append Number of sentences -> estimated
-            if(l == ""):
-                data.append("tbd")
-            else:
-                data.append(str(l.count(".<")+l.count("?<")+l.count("!<")+l.count(". ")+l.count("? ")+l.count("! ")))
-             
-            # append Date of Acquisition
-            data.append(str(datetime.datetime.now().strftime("%Y/%m/%d")))
-            
-            # append File size
-            data.append(str(stream.filesize_mb)+" MB")
-            
-            # append Video Path
-            data.append(Lang+"\\"+p_t+"\\"+"videos_uncropped\\"+titel+".mp4")
-    
-    
-    
-    # write csv-file
-    sv.writecsv( "new.csv", "|", titles, data)
-    print("\nwrote csv-file")
+                    stream = yt.streams.get_highest_resolution()
+                    yt.streams.get_highest_resolution().download(os.path.join(p_t , "videos_uncropped"),titel+".mp4")
+                except:
+                    print(f'\nDownloading {url} is unavaialable, skipping.')
+                else: 
+                    #download captions
+                    l = ""
+                    lang_code = lang_code_s
+                    while(l == ""):
+                        if(lang_code == "" or len(yt.captions) == 0):
+                            break
+                        elif(len(yt.captions) == 1):
+                            lang_code = list(yt.captions.lang_code_index.keys())[0]
+                            l = yt.captions[lang_code].xml_captions
+                            d = open(os.path.join(p_t, "captions", titel+"_"+lang_code+".xml"), "x", encoding="utf-8")
+                            d.write(l)
+                            d.close
+                        else:
+                            try:
+                                l = yt.captions[lang_code].xml_captions
+                                d = open(os.path.join(p_t, "captions", titel+"_"+lang_code+".xml"), "x", encoding="utf-8")
+                                d.write(l)
+                                d.close
+                            except:
+                                print("\n"+str(yt.captions))
+                                lang_code = input("\nType in the language code for the captions or press enter: ")
+                                
+        
+                    # append Channel Id -> not codable
+                    data.append(C_ID)
+                    
+                    # append Language -> not codable
+                    data.append(Lang)
+                    
+                    # append Channel Dialect -> not codable
+                    data.append(dia)
+                    
+                    # append Channel Video Name
+                    data.append(str(titel))
+                    
+                    # append Part of Series -> not codable
+                    data.append("tbd")
+                    
+                    # append Url
+                    data.append(str(url))
+                    
+                    # append Release Date
+                    data.append(str(YouTube(url).publish_date.strftime("%Y/%m/%d")))
+                    
+                    # append Resolution
+                    data.append(str(stream.resolution))
+                    
+                    # append Cropped -> not codable
+                    data.append("tbd")
+                    
+                    # append Video Length
+                    i = 0
+                    while(i < 3):
+                        try:
+                            t = YouTube(url).length
+                            t_form = str(time.strftime("%H:%M:%S", time.gmtime(t)))
+                            i = 3
+                        except Exception:
+                            t_form = "tbd"
+                        i = i+1    
+                    data.append(t_form)
+                    
+                    # append FPS
+                    data.append(str(stream.fps))
+                    
+                    # append Number of Frames
+                    if(t_form != "tbd"):
+                        data.append(str(stream.fps*t))
+                    else:
+                        data.append("tbd")
+                    
+                    # append SL Subtitles Available -> not codable
+                    data.append("tbd")
+                    
+                    # append Audio Transcript Available -> not codable
+                    data.append("tbd")
+                    
+                    # append Granularity -> not codable
+                    data.append(gran)
+                    
+                    # append Number of sentences -> estimated
+                    if(l == ""):
+                        data.append("tbd")
+                    else:
+                        data.append(str(l.count(".<")+l.count("?<")+l.count("!<")+l.count(". ")+l.count("? ")+l.count("! ")))
+                     
+                    # append Date of Acquisition
+                    data.append(str(datetime.datetime.now().strftime("%Y/%m/%d")))
+                    
+                    # append File size
+                    data.append(str(stream.filesize_mb)+" MB")
+                    
+                    # append Video Path
+                    data.append(Lang+"\\"+p_t+"\\"+"videos_uncropped\\"+titel+".mp4")
+        
+        
+        
+        # write csv-file
+        sv.writecsv( "new.csv", "|", titles, data)
+        print("\nwrote csv-file")
+    except Exception as e:
+        print("\nThe error raised is: ", e)
+        # write csv-file
+        sv.writecsv( "new.csv", "|", titles, data)
+        print("\nwrote csv-file after error")
+        rep = input("\nDo you want to repeat with the same data? (y/n) ")
+        if(rep == "y"):
+            createdata(C_ID, Lang, dia, gran, a, lang_code_s, cp)
+        
     
 
 
 
 titles = ["Channel Id",	"Language",	"Dialect","Video Name","Part of Series","Url","Release Date","Resolution","Cropped","Video Length","FPS","Number of Frames","SL Subtitles Available","Audio Transcript Available","Granularity","Number of Sentences","Date of Acquisition","File Size","Video Path"]
-data = []
+data_ = []
 
 
 C_ID = input("\nType in the Channel ID from the GoogleSheet: ")
--- scraping_Youtube/YTgetc.py
+++ scraping_Youtube/YTgetc.py
@@ -32,146 +32,163 @@ def createnewtitle(direc,name):
         
 
 def createdata(C_ID, Lang, dia, gran, a, lang_code, cp):
+    data = data_
+    try:
     
-    p,p_t = geturl(cp, a)
-    
-    if(os.path.exists(p_t) == False):
-        os.mkdir(p_t)
-    else:
-        shutil.rmtree(p_t)
-        os.mkdir(p_t)
-    
-    os.mkdir(os.path.join(p_t , "captions"))
-    os.mkdir(os.path.join(p_t , "videos"))
-    os.mkdir(os.path.join(p_t , "videos_uncropped"))
-    os.mkdir(os.path.join(p_t , "other"))
-    
-    for url in p.video_urls:
-        try:
-            yt = YouTube(url)
-        except:
-            print(f'\nVideo {url} is unavaialable, skipping.')
+        p,p_t = geturl(cp, a)
+        
+        if(os.path.exists(p_t) == False):
+            os.mkdir(p_t)
         else:
-            
-            #create variable for the title, which can used as filename
-            titel = yt.title.replace("|", "").replace("\\", "") \
-                        .replace("/", "").replace("\"", "").replace("?", "") \
-                        .replace("*", "").replace(":", "").replace(">", "").replace("<", "")
-            
-            titel = createnewtitle(p_t, titel)
-            #download captions
-            l = ""
-            lang_code = lang_code_s
-            while(l == ""):
-                if(lang_code == "" or len(yt.captions) == 0):
-                    break
-                elif(len(yt.captions) == 1):
-                    lang_code = list(yt.captions.lang_code_index.keys())[0]
-                    l = yt.captions[lang_code].xml_captions
-                    d = open(os.path.join(p_t, "captions", titel+"_"+lang_code+".xml"), "x", encoding="utf-8")
-                    d.write(l)
-                    d.close
-                else:
-                    try:
+            rem = input("\nDirectory for this URL already exist. Do you want to remove it? (y/n) ")
+            if(rem == "y"):
+                shutil.rmtree(p_t)
+                os.mkdir(p_t)
+                
+        
+        os.makedirs(os.path.join(p_t , "captions"), exist_ok = True)
+        os.makedirs(os.path.join(p_t , "videos"), exist_ok = True)
+        os.makedirs(os.path.join(p_t , "videos_uncropped"), exist_ok = True)
+        os.makedirs(os.path.join(p_t , "other"), exist_ok = True)
+        
+        for url in p.video_urls:
+            try:
+                yt = YouTube(url)
+            except:
+                print(f'\nVideo {url} is unavaialable, skipping.')
+            else:
+                
+                #create variable for the title, which can used as filename
+                titel = yt.title.replace("|", "").replace("\\", "") \
+                            .replace("/", "").replace("\"", "").replace("?", "") \
+                            .replace("*", "").replace(":", "").replace(">", "").replace("<", "")
+                
+                titel = createnewtitle(p_t, titel)
+                
+                cap_tit = os.path.join(p_t, "captions", titel+"_"+lang_code+".xml")
+                if(os.path.exists(cap_tit) == True):
+                    print("Video is already downloaded.")
+                    continue
+                #download captions
+                l = ""
+                lang_code = lang_code_s
+                while(l == ""):
+                    if(lang_code == "" or len(yt.captions) == 0):
+                        break
+                    elif(len(yt.captions) == 1):
+                        lang_code = list(yt.captions.lang_code_index.keys())[0]
                         l = yt.captions[lang_code].xml_captions
-                        d = open(os.path.join(p_t, "captions", titel+"_"+lang_code+".xml"), "x", encoding="utf-8")
+                        d = open(str(cap_tit), "x", encoding="utf-8")
                         d.write(l)
                         d.close
-                    except:
-                        print("\n"+str(yt.captions))
-                        lang_code = input("\nType in the language code for the captions or press enter: ")
-                        
-            if(l == ""):
-                print(f'\nDownloading captions from {url} is not possible.')
-            else:
-                print(f'\nDownloading video: {url}')
-                
-                #download the video
-                try:
-                    stream = yt.streams.get_highest_resolution()
-                    yt.streams.get_highest_resolution().download(os.path.join(p_t , "videos_uncropped"),titel+".mp4")
-                except:
-                    print(f'\nDownloading {url} is unavaialable, skipping.')
-                  
-    
-                # append Channel Id -> not codable
-                data.append(C_ID)
-                
-                # append Language -> not codable
-                data.append(Lang)
-                
-                # append Channel Dialect -> not codable
-                data.append(dia)
-                
-                # append Channel Video Name
-                data.append(str(titel))
-                
-                # append Part of Series -> not codable
-                data.append("tbd")
-                
-                # append Url
-                data.append(str(url))
-                
-                # append Release Date
-                data.append(str(YouTube(url).publish_date.strftime("%Y/%m/%d")))
-                
-                # append Resolution
-                data.append(str(stream.resolution))
-                
-                # append Cropped -> not codable
-                data.append("tbd")
-                
-                # append Video Length
-                i = 0
-                while(i < 3):
-                    try:
-                        t = YouTube(url).length
-                        t_form = str(time.strftime("%H:%M:%S", time.gmtime(t)))
-                        i = 3
-                    except Exception:
-                        t_form = "tbd"
-                    i = i+1    
-                data.append(t_form)
-                
-                # append FPS
-                data.append(str(stream.fps))
-                
-                # append Number of Frames
-                if(t_form != "tbd"):
-                    data.append(str(stream.fps*t))
-                else:
-                    data.append("tbd")
-                
-                # append SL Subtitles Available -> not codable
-                data.append("tbd")
-                
-                # append Audio Transcript Available -> not codable
-                data.append("tbd")
-                
-                # append Granularity -> not codable
-                data.append(gran)
-                
-                # append Number of sentences -> estimated
+                    else:
+                        try:
+                            l = yt.captions[lang_code].xml_captions
+                            d = open(str(cap_tit), "x", encoding="utf-8")
+                            d.write(l)
+                            d.close
+                        except:
+                            print("\n"+str(yt.captions))
+                            lang_code = input("\nType in the language code for the captions or press enter: ")
+                            
                 if(l == ""):
-                    data.append("tbd")
+                    print(f'\nDownloading captions from {url} is not possible.')
                 else:
-                    data.append(str(l.count(".<")+l.count("?<")+l.count("!<")+l.count(". ")+l.count("? ")+l.count("! ")))
-                 
-                # append Date of Acquisition
-                data.append(str(datetime.datetime.now().strftime("%Y/%m/%d")))
-                
-                # append File size
-                data.append(str(stream.filesize_mb)+" MB")
-                
-                # append Video Path
-                data.append(Lang+"\\"+p_t+"\\"+"videos_uncropped\\"+titel+".mp4")
+                    print(f'\nDownloading video: {url}')
+                    
+                    #download the video
+                    try:
+                        stream = yt.streams.get_highest_resolution()
+                        yt.streams.get_highest_resolution().download(os.path.join(p_t , "videos_uncropped"),titel+".mp4")
+                    except:
+                        print(f'\nDownloading {url} is unavaialable, skipping.')
+                      
+        
+                    # append Channel Id -> not codable
+                    data.append(C_ID)
+                    
+                    # append Language -> not codable
+                    data.append(Lang)
+                    
+                    # append Channel Dialect -> not codable
+                    data.append(dia)
+                    
+                    # append Channel Video Name
+                    data.append(str(titel))
+                    
+                    # append Part of Series -> not codable
+                    data.append("tbd")
+                    
+                    # append Url
+                    data.append(str(url))
+                    
+                    # append Release Date
+                    data.append(str(YouTube(url).publish_date.strftime("%Y/%m/%d")))
+                    
+                    # append Resolution
+                    data.append(str(stream.resolution))
+                    
+                    # append Cropped -> not codable
+                    data.append("tbd")
+                    
+                    # append Video Length
+                    i = 0
+                    while(i < 3):
+                        try:
+                            t = YouTube(url).length
+                            t_form = str(time.strftime("%H:%M:%S", time.gmtime(t)))
+                            i = 3
+                        except Exception:
+                            t_form = "tbd"
+                        i = i+1    
+                    data.append(t_form)
+                    
+                    # append FPS
+                    data.append(str(stream.fps))
+                    
+                    # append Number of Frames
+                    if(t_form != "tbd"):
+                        data.append(str(stream.fps*t))
+                    else:
+                        data.append("tbd")
+                    
+                    # append SL Subtitles Available -> not codable
+                    data.append("tbd")
+                    
+                    # append Audio Transcript Available -> not codable
+                    data.append("tbd")
+                    
+                    # append Granularity -> not codable
+                    data.append(gran)
+                    
+                    # append Number of sentences -> estimated
+                    if(l == ""):
+                        data.append("tbd")
+                    else:
+                        data.append(str(l.count(".<")+l.count("?<")+l.count("!<")+l.count(". ")+l.count("? ")+l.count("! ")))
+                     
+                    # append Date of Acquisition
+                    data.append(str(datetime.datetime.now().strftime("%Y/%m/%d")))
+                    
+                    # append File size
+                    data.append(str(stream.filesize_mb)+" MB")
+                    
+                    # append Video Path
+                    data.append(Lang+"\\"+p_t+"\\"+"videos_uncropped\\"+titel+".mp4")
     
     
     
-    
-    # write csv-file
-    sv.writecsv( "new.csv", "|", titles, data)
-    print("\nwrote csv-file")
+        # write csv-file
+        sv.writecsv( "new.csv", "|", titles, data)
+        print("\nwrote csv-file")
+    except Exception as e:
+        print("\nThe error raised is: ", e)
+        # write csv-file
+        sv.writecsv( "new.csv", "|", titles, data)
+        print("\nwrote csv-file after error")
+        rep = input("\nDo you want to repeat with the same data? (y/n) ")
+        if(rep == "y"):
+            createdata(C_ID, Lang, dia, gran, a, lang_code_s, cp)
     
 
 
